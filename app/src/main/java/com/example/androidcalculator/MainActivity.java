ab5c20766075e54bc42e09bcb0c29398407b37e0
shakthishamruth <shakthishamruth@gmail.com>
2023-06-23 22:56:16 +0530

    Added Onclick

--- app/src/main/java/com/example/androidcalculator/MainActivity.java
+++ app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidcalculator;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator_layout);
+    }
+
+    public void onClickC(View view) {
+    }
+
+    public void onClickBack(View view) {
+    }
+
+    public void onClick0(View view) {
+    }
+
+    public void onClick1(View view) {
+    }
+
+    public void onClick2(View view) {
+    }
+
+    public void onClick3(View view) {
+    }
+
+    public void onClick4(View view) {
+    }
+
+    public void onClick5(View view) {
+    }
+
+    public void onClick6(View view) {
+    }
+
+    public void onClick7(View view) {
+    }
+
+    public void onClick8(View view) {
+    }
+
+    public void onClick9(View view) {
+    }
+
+    public void onClickMinus(View view) {
+    }
+
+    public void onClickPlus(View view) {
+    }
+
+    public void onClickEquals(View view) {
+    }
+
+    public void onClickMultiply(View view) {
+    }
+
+    public void onClickDivide(View view) {
+    }
+
+    public void onClickDot(View view) {
     }
 }
